2c14584600586bf6ddcb7dba1234a955d93850ed
Alex Popovici <popovici.alex96@gmail.com>
2022-05-21 21:38:21 +0300

    Setup app

---
+++ Server/Controllers/QuizzController.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace Server.Controllers
+{
+    public class QuizzController : ApiController
+    {
+        public static HttpClient apiClient { get; set; } = new HttpClient();
+        [HttpGet]
+        [Route ("hello")]
+        public async Task<IHttpActionResult> Get()
+        {
+            apiClient.DefaultRequestHeaders.Accept.Clear();
+            apiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            using (var response = await apiClient.GetAsync("https://opentdb.com/api.php?amount=1"))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    string data = await response.Content.ReadAsStringAsync();
+                    Console.Write(data);
+                    Quizz quizz = JsonConvert.DeserializeObject<Quizz>(data);
+                    return Ok(quizz);
+                }
+            }
+            return NotFound();
+        }
+
+    }
+}
---
+++ Server/Quizz.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Server
+{
+    public class Question
+    {
+        /* {
+            "category": "Entertainment: Television",
+            "type": "multiple",
+            "difficulty": "medium",
+            "question": "In the original Doctor Who series (1963), fourth doctor Tom Baker&#039;s scarf was how long?",
+            "correct_answer": "7 Meters",
+            "incorrect_answers": [
+                "10 Meters",
+                "2 Meters",
+                "5 Meters"
+            ]
+        },*/
+        public string category { get; set; }
+        public string type { get; set; }
+        public string difficulty { get; set; }
+        public string question { get; set; }
+        public string correct_answer { get; set; }
+
+        public List<string> incorrect_answers { get; set; }
+
+    }
+
+    public class Quizz
+    {
+        public string response_code { get; set; }
+        public List<Question> results { get; set; }
+    }
+}
